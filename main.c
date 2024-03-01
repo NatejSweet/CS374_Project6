82dadd0da2289e88a387ec6bd26f77baf1eb06fa
Nathan Sweet <nathan.j.sweet852@gmail.com>
2024-03-01 15:33:33 -0800

    project 8 complete

--- main.c
+++ main.c
@@ -26,15 +26,16 @@ void *myalloc(int size)
     {
         heap = mmap(NULL, 1024, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
         head = (struct block *)heap;
-        struct block *next_block = PTR_OFFSET(head, PADDED_SIZE(sizeof(struct block)) + actual_size);
         head->size = data_size;
-        next_block->size = head->size - actual_size - PADDED_SIZE(sizeof(struct block));
         head->in_use = 0;
-        head->next = next_block;
+        head->next = NULL;
         first_call = 0;
     }
-    if (head->in_use == 0 && head->size >= actual_size)
+    if (head->in_use == 0 && head->size >= (int)(actual_size+PADDED_SIZE(sizeof(struct block))))       //if the first block is free and has enough space
     {
+        struct block *next_block = PTR_OFFSET(head, PADDED_SIZE(sizeof(struct block)) + actual_size);   
+        next_block->size = head->size - actual_size - PADDED_SIZE(sizeof(struct block));
+        head->next = next_block;
         head->in_use = 1;
         head->size = actual_size;
         return (int *)PTR_OFFSET(head, sizeof(struct block));
@@ -42,24 +43,24 @@ void *myalloc(int size)
     else
     {
         struct block *current = head;
-        while (current->next != NULL && (current->in_use == 1 || current->size < actual_size))
+        while (current->next != NULL && (current->in_use == 1 || current->size < actual_size))  //if the first block is not free or does not have enough space
         {
             current = current->next;
         }
-        if (current->size < actual_size)//ensure there is room(may need to add size of another node, not sure, works for now)
+        if (current->size < actual_size || current->in_use == 1)//if no space is available
         {
-            printf("No space available\n");
+            printf("No space available\n"); //another heap could be allocated here to allow limitless allocation(beyond 1024 bytes)
             return NULL;
         }
         else{
-            if (current->next == NULL)
+            if (current->next == NULL && current->size >= (int)(actual_size+PADDED_SIZE(sizeof(struct block))))  //if is the last block and has enough space
             {
-                struct block *next_block = PTR_OFFSET(current, PADDED_SIZE(sizeof(struct block)) + actual_size); // next_block is the new block
-                next_block->size = current->size - actual_size - PADDED_SIZE(sizeof(struct block)); // size of the new block = size of the current block - size of the requested block - size of the block struct
+                struct block *next_block = PTR_OFFSET(current, PADDED_SIZE(sizeof(struct block)) + actual_size);
+                next_block->size = current->size - actual_size - PADDED_SIZE(sizeof(struct block)); 
                 current->in_use = 1;
-                current->size = actual_size; // size of the current block = size of the requested block
-                current->next = next_block;  // next of the current block = next_block
-                return (int *)PTR_OFFSET(current, sizeof(struct block));
+                current->size = actual_size;
+                current->next = next_block; 
+                return (int *)PTR_OFFSET(current, sizeof(struct block));    
             }
             else
             {
@@ -71,19 +72,36 @@ void *myalloc(int size)
 }
 
 int myfree(int *ptr)
-{   ptr = (int *)PTR_OFFSET(ptr, -sizeof(struct block));
+{
     struct block *current = head;
     while (current != NULL)
     {
-        if (current == ptr)
+        if ((int *)PTR_OFFSET(current, sizeof(struct block)) == ptr)//if current is the target
         {
             current->in_use = 0;
-            return 0;
+            return coalesceFrom(head);
         }
         current = current->next;
     }
     return -1;
 }
+
+int coalesceFrom(struct block *start)
+{
+    struct block *current = start;
+    while (current->next != NULL)
+    {
+        if (current->in_use == 0 && current->next->in_use == 0) //if both current and next are free
+        {
+            current->size += current->next->size + PADDED_SIZE(sizeof(struct block));
+            current->next = current->next->next;
+            return coalesceFrom(current);   //recursively coalesce, from the same node in case of multiple free blocks in a row. 
+        }
+        current = current->next;
+    }
+    return 0;
+}
+
 
 void print_data(void)
 {
@@ -113,12 +131,15 @@ void print_data(void)
 
 int main(int argc, char *argv[])
 {
-void *p;
+void *p, *q, *r, *s;
 
-myalloc(10);     print_data();
-p = myalloc(20); print_data();
-myalloc(30);     print_data();
-myfree(p);       print_data();
-myalloc(40);     print_data();
-myalloc(10);     print_data();
+p = myalloc(10); print_data();
+q = myalloc(20); print_data();
+r = myalloc(30); print_data();
+s = myalloc(40); print_data();
+
+myfree(q); print_data();
+myfree(p); print_data();
+myfree(s); print_data();
+myfree(r); print_data();
 }
